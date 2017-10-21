318a00d9790a36e58dd7581384d609f0a1c44a33
YuHashi <lacrosse-otafuku@hotmail.co.jp>
2017-10-21 14:49:19 +0900

    初回_アカウント作成ができない

---
+++ app/src/main/java/jp/techacademy/yuto/hashiba/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.yuto.hashiba.qa_app;
+
+/**
+ * Created by Yuto on 2017/10/15.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
